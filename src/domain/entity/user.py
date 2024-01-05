6257e71a00e687c98679c75aa3e67e38ee8a54ac
Yury Tomaz <contato@viladev.com>
2024-01-05 17:05:02 -0300

    feat: add User entity class

---
+++ src/domain/entity/user.py
@@ -0,0 +1,8 @@
+# User Entity
+
+class User:
+    def __init__(self, id: int, first_name: str, last_name: str,  age: int):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
